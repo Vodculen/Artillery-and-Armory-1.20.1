9fd10b2111f1bc1bd17df8262c80c63d9b10857b
Vodculen <stefanaschen@gmail.com>
2025-03-24 16:39:19 -0400

    Began work on Noavas Mod.

---
+++ src/main/java/net/vodculen/artilleryandarmory/ArtilleryArmory.java
@@ -0,0 +1,16 @@
+package net.vodculen.artilleryandarmory;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import net.fabricmc.api.ModInitializer;
+
+public class ArtilleryArmory implements ModInitializer {
+	public static final String MOD_ID = "artilleryandarmory";
+	public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
+
+	@Override
+	public void onInitialize() {
+
+	}
+}
---
+++ src/main/java/net/vodculen/artilleryandarmory/ArtilleryArmoryClient.java
@@ -0,0 +1,11 @@
+package net.vodculen.artilleryandarmory;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class ArtilleryArmoryClient implements ClientModInitializer {
+	@Override
+	public void onInitializeClient() {
+
+	}
+	
+}
